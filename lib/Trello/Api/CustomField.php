e96ed730070210c8e52283c5cb9e8257027f64c3
Scott Teglasi <scott.teglasi@gmail.com>
2019-03-23 08:06:03 -0400

    Add api for custom field (UNTESTED)

---
+++ lib/Trello/Api/CustomField.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scott
+ * Date: 23/03/19
+ * Time: 7:57 AM
+ */
+
+namespace Trello\Api;
+
+
+class CustomField extends AbstractApi
+{
+    protected $path = 'customFields';
+
+    public static $fields = [
+        'id',
+        'idModel',
+        'modelType',
+        'fieldGroup',
+        'name',
+        'pos',
+        'type',
+        'options',
+        'display'
+    ];
+
+    public function show($id, array $params = [])
+    {
+        return $this->get($this->getPath() . '/' . rawurlencode($id), $params);
+    }
+
+    public function create(array $params = [])
+    {
+        $this->validateRequiredParameters(array('name', 'idModel', 'modelType', 'name', 'type', 'pos'), $params);
+
+        return $this->post($this->getPath(), $params);
+    }
+
+    public function update($id, array $params = [])
+    {
+        return $this->put($this->getPath() . '/' . rawurlencode($id), $params);
+    }
+}
